cf2662b8f18015957280c40e6b0fcd6af551ddbc
Roberto <roberto.zamudio77@gmail.com>
2022-09-23 11:51:28 -0500

    cambio de 100 a 200

--- videojuego.py
+++ videojuego.py
@@ -2,9 +2,9 @@ import random
 
 
 def run():
-    numero_aleatorio = random.randint(1, 100)
+    numero_aleatorio = random.randint(1, 200)
     numero_elegido = int(input("Elige un número del 1 al 100: "))
-    if numero_elegido > 100:
+    if numero_elegido > 200:
         print ("Elige un numero entre 1 y 100")
     numero_elegido=int(input("Elige otro número: "))
     while numero_elegido!=numero_aleatorio:
