11659897b573431939ebaa1cf740f7e92bde3738
Roberto <roberto.zamudio77@gmail.com>
2022-09-23 11:55:22 -0500

    segundo cambio

--- videojuego.py
+++ videojuego.py
@@ -3,9 +3,9 @@ import random
 
 def run():
     numero_aleatorio = random.randint(1, 200)
-    numero_elegido = int(input("Elige un número del 1 al 100: "))
+    numero_elegido = int(input("Elige un número del 1 al 200: "))
     if numero_elegido > 200:
-        print ("Elige un numero entre 1 y 100")
+        print ("Elige un numero entre 1 y 200")
     numero_elegido=int(input("Elige otro número: "))
     while numero_elegido!=numero_aleatorio:
         if numero_elegido < numero_aleatorio:
